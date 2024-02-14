c23a37e0fb5017b4d60bb8521fa010776b362afc
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-02-14 21:09:21 +0900

    Initial Commit

---
+++ iPhoneReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iPhoneReminder
+//
+//  Created by hwijinjeong on 2/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
